5cdcb24f6cfd3b3e38652a0329ff7ed894aa9984
hasnattanvir <hasnattanvir014@gmail.com>
2026-04-14 10:05:28 +0600

    Implement caching for product data retrieval in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
@@ -14,14 +15,15 @@ class ProductController extends Controller
     }
 
 
+    // Location API Free Cash Data
     public function products(){
        try{
             $products = [];
-            $apiKey = config('services.new-api.key');
-            // dd($apiKey);
-            // x-api-key: pub_facf1875a8c0fb821f104ecfa14a48af
-            $response = Http::withHeader('x-api-key', 'pub_facf1875a8c0fb821f104ecfa14a48af')->get('https://reqres.in/api/users');
-            $products = $response->json();
+            $response = Cache::remember('product', now()->addMinutes(2), function () {
+               return Http::get('http://ip-api.com/json/24.48.0.1')->json();
+            });
+
+            $products = $response ?? [];
             dd($products);
             
        }catch(\Exception $ex){
@@ -34,6 +36,50 @@ class ProductController extends Controller
         // dd($products);
         return view('products.index', compact('products'));
     }
+
+    // Location API Free
+    // public function products(){
+    //    try{
+    //         $products = [];
+    //         // $response = Http::get('http://ip-api.com/json');
+    //         $response = Http::get('http://ip-api.com/json',[
+    //             'query' => ''
+    //         ]);
+    //         $products = $response->json();
+    //         dd($products);
+            
+    //    }catch(\Exception $ex){
+    //         return response()->json([
+    //                 'error' => 'API Respones Faild ',
+    //                 'status' =>401
+    //             ],401);
+    //    }
+
+    //     // dd($products);
+    //     return view('products.index', compact('products'));
+    // }
+
+    // Paid API
+    // public function products(){
+    //    try{
+    //         $products = [];
+    //         $apiKey = config('services.new-api.key');
+    //         // dd($apiKey);
+    //         // x-api-key: pub_facf1875a8c0fb821f104ecfa14a48af
+    //         $response = Http::withHeader('x-api-key', 'pub_facf1875a8c0fb821f104ecfa14a48af')->get('https://reqres.in/api/users');
+    //         $products = $response->json();
+    //         dd($products);
+            
+    //    }catch(\Exception $ex){
+    //         return response()->json([
+    //                 'error' => 'API Respones Faild ',
+    //                 'status' =>401
+    //             ],401);
+    //    }
+
+    //     // dd($products);
+    //     return view('products.index', compact('products'));
+    // }
 
     // free api
     // public function products(){
